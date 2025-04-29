cfda5b2644f2e93359d4cd710a0d40ba27799a8d
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-28 22:12:52 -0700

    button styling

--- Frontend/src/components/TenantDashboard/LandlordApplications.jsx
+++ Frontend/src/components/TenantDashboard/LandlordApplications.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import '../../styles/LandlordApplications.css'
+import '../../styles/buttonStyles.css'
 
 const LandlordApplications = () => {
   const [applications, setApplications] = useState([]);
