71389c852b786c7fbfbc5818b2eea531e6078267
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-28 22:07:04 -0700

    button focus and hover effects

--- Frontend/src/components/TenantDashboard/LandlordApplications.jsx
+++ Frontend/src/components/TenantDashboard/LandlordApplications.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import '../../styles/LandlordApplications.css'
 
 const LandlordApplications = () => {
   const [applications, setApplications] = useState([]);
@@ -105,7 +106,7 @@ const LandlordApplications = () => {
                 width: "100%",
                 marginTop: "0.5rem",
                 padding: "0.5rem",
-                backgroundColor: "#007bff",
+                backgroundColor: "#388697",
                 color: "white",
                 border: "none",
                 borderRadius: "4px",
@@ -117,10 +118,14 @@ const LandlordApplications = () => {
             </button>
 
             <div style={{ marginTop: "0.5rem" }}>
-              <button onClick={() => handleAccept(app.id)} style={{ marginRight: "1rem" }}>
+              <button 
+                className="acceptButton"
+                onClick={() => handleAccept(app.id)} style={{ marginRight: "1rem" }}>
                 Accept
               </button>
-              <button onClick={() => handleReject(app.id)}>
+              <button 
+                className="rejectButton"
+                onClick={() => handleReject(app.id)}>
                 Reject
               </button>
             </div>
